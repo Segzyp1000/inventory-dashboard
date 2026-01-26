9d598c9029581a267dfce552cfe1d83f3b1a6afc
Segun Olowoyeye <segunolowoyeye@gmail.com>
2026-01-26 14:58:47 +0100

    workedon the add-page comp

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -113,7 +113,7 @@ export default async function AddProductPage() {
               <div className="flex  flex-wrap gap-5">
                 <button
                   type="submit"
-                  className="px-6 py-3 bg-purple-600 text-white rounded-lg hover:bg-purple-700"
+                  className="px-6 py-3 bg-purple-600 text-white rounded-lg hover:bg-purple-700 space-y cursor-pointer"
                 >
                   Add Product
                 </button>
